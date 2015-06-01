59baaff53641564cdf085b0539aba243e922c18b
shadow_walker <shadowwalkersb@gmail.com>
2015-06-01 12:41:17 -0500

    remove commented out from download step

--- boost/Boost.cmake
+++ boost/Boost.cmake
@@ -18,15 +18,11 @@ include(ExternalProject)
 ExternalProject_Add( Bust
 	PREFIX ${CMAKE_SOURCE_DIR}/ExternalLibraries/boost_boost
  #--Download step--------------
- # # [DOWNLOAD_NAME fname]       # File name to store (if not end of URL)
  #  DOWNLOAD_DIR   ${CMAKE_SOURCE_DIR}/ExternalLibraries/boost_src        # Directory to store downloaded files
  #  DOWNLOAD_COMMAND ""   # Command to download source tree
  #   URL          http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz # Full path or URL of source
  #   URL_HASH SHA1=a27b010b9d5de0c07df9dddc9c336767725b1e6b       # Hash of file at URL
  #   URL_MD5 5a5d5614d9a07672e1ab2a250b5defc5 # Equivalent to URL_HASH MD5=md5
- #   #[TLS_VERIFY bool]           # Should certificate for https be checked
- #   #[TLS_CAINFO file]           # Path to a certificate authority file
- #   #[TIMEOUT seconds]           # Time allowed for file download operations
  #--Configure step-------------
  SOURCE_DIR ${Source_Dir}            # Source dir to be used for build
  CONFIGURE_COMMAND  ${Source_Dir}/bootstrap.sh --prefix=${CMAKE_SOURCE_DIR}/ExternalLibraries/boost_out/ --with-libraries=python # Build tree configuration command
