150341f8c441b96577f0dccaa009eb6d55938b9e
shadow_walker <shadow_walker@bitbucket.org>
2015-06-05 17:17:09 -0500

    fix glut

--- GLUT.cmake
+++ GLUT.cmake
@@ -1,19 +1,34 @@
 
-set(ext_dir ${CMAKE_SOURCE_DIR}/ExternalLibraries/GL/${folder_name}/GL)
+set(ext_dir ${CMAKE_SOURCE_DIR}/ExternalLibraries/GL/${folder_name})
 
-#message("          VAL: ${ext_dir}")
+message("          GLUT: ${ext_dir}")
+
+#if(NOT WIN32)
+#    set( include_folder "${ext_dir}/include" )
+#    set( lib_folder "${ext_dir}/lib" )
+#else()
+#    set( include_folder ${ext_dir} )
+#    set( lib_folder     ${ext_dir} )
+#endif()
+
+set( include_folder "${ext_dir}/include/GL" )
+set( lib_folder "${ext_dir}/lib" )
+
+message("          GLUT include: ${include_folder}")
+message("          GLUT lib    : ${lib_folder}")
+
 
 FIND_PATH(
     GLUT_INCLUDE_DIR
     NAMES glut.h
-    PATHS ${ext_dir}/include
+    PATHS ${include_folder}
     #NO_DEFAULT_PATH
 )
 
 FIND_LIBRARY(
     GLUT_glut_LIBRARY
     NAMES freeglut
-    PATHS ${ext_dir}/lib
+    PATHS ${lib_folder}
     #NO_DEFAULT_PATH
 )
 
